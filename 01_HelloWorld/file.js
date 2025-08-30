4694feaca0d901b51358019dd3d3dac3be9d33e9
Priyanka-saw <priyankasaw51@gmail.com>
2025-08-30 23:47:56 +0530

    initial update

--- 01_HelloWorld/file.js
+++ 01_HelloWorld/file.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
+const os = require('os');
 
+console.log(os.cpus().length);
 
-// Sync...
+// Sync...Blocking operations
 // fs.writeFileSync("./test.txt", "hello world");
 
 
-// Async
+// Async -: Non-Blocking Operations
 // fs.writeFile("./test.txt", "hello world Async", (err) => {});
 
 
@@ -26,17 +28,39 @@ const fs = require("fs");
 
 // fs.appendFileSync('./test.txt', new Date().getDate().toLocaleString());
 
-// for next line
-fs.appendFileSync('./test.txt', 'nameste india\n');
+// // for next line
+// fs.appendFileSync('./test.txt', 'nameste india\n');
 
 
-fs.copyFileSync('./test.txt', './copyfile');
+// fs.copyFileSync('./test.txt', './copyfile');
 
-console.log(fs.statSync('./test.txt').isFile());
+// console.log(fs.statSync('./test.txt').isFile());
 
 
 
+// Synchronous
+// console.log("priyanka");
+
+// const result = fs.readFileSync('./contacts.txt', 'utf-8')
+// console.log(result);
+
+// console.log("hello priyanka");
+
+
+
+// Asynchronus
+console.log("priyanka");
+
+fs.readFile('./contacts.txt', 'utf-8', (err, result) =>{
+    console.log(result);
+    
+})
+
+console.log("hello priyanka");
 
 
 // fs stand for file system
 // utf-8 : file name encodeing
+
+// default Thread pool Size = 4
+// Max? - 8core cpu - 8
